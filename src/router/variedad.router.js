2a2ec4c47fefda24013aa8eeb2a4cb87dc15b2bd
Libni Bernate <libnibernate@gmail.com>
2024-08-12 11:59:32 -0500

    actualizado router y controlador de VARIEDAD

---
+++ src/router/variedad.router.js
@@ -0,0 +1,141 @@
+import express from "express";
+import { Variedades } from "../models/variedad.model.js";
+
+const variedadesRouter = express.Router()
+
+// create variedad
+variedadesRouter.post('/variedad', async (req, res) => {
+
+  const {nombre} = req.body;
+
+    try {
+      const variedad = await Variedades.create({nombre});
+
+      res.status(201).json({
+        message: "Variedad creada correctamente",
+        variedad
+      });
+
+    }catch (error) {
+      res.status(500).json({ 
+        message:"Error al crear la variedad",
+        error: error.message
+      });
+    }
+  });
+  
+
+// get all variedad
+variedadesRouter.get('/variedad', async (req, res) => {
+
+    try {
+      const variedades = await Variedades.find();
+
+      res.status(200).json({
+        message: "variedades encontradas correctamente",
+        variedades
+      });
+
+    }catch (error) {
+      res.status(500).json({
+        message:"Error al encontrar las variedades",
+        error: error.message
+      });
+    }
+  });
+  
+
+// get a variedad
+variedadesRouter.get('/variedad/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const variedad = await Variedades.findById(id);
+
+    //verificar si la variedad existe
+    if (!variedad) {
+      return res.status(404).json({ 
+        message: "Variedad no encontrada con ese id",
+      });
+    }
+    
+    res.status(200).json({
+      message: "Variedad encontrada correctamente",
+      variedad
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al obtener variedad",
+      error: error.message
+    });
+  }
+});
+
+  
+
+// uptade a variedad
+variedadesRouter.put('/variedad/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { nombre } = req.body;
+    
+    // Verificar si la variedad existe
+    const variedadExistente = await Variedades.findById(id);
+
+    if (!variedadExistente) {
+      return res.status(404).json({
+        message: "ID no encontrado. No se puede actualizar."
+      });
+    }
+
+    // Actualizar la variedad
+    const updatedVariedad = await Variedades.updateOne({ _id: id }, { $set: { nombre } });
+
+    res.status(200).json({
+      message: "Variedad actualizada correctamente",
+      updatedVariedad
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al actualizar variedad",
+      error: error.message
+    });
+  }
+});
+
+  
+
+// delete a variedad
+variedadesRouter.delete('/variedad/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Verificar si la variedad existe 
+    const variedadExistente = await Variedades.findById(id);
+
+    if (!variedadExistente) {
+      return res.status(404).json({
+        message: "ID no encontrado. No se puede eliminar."
+      });
+    }
+
+    // Eliminar la variedad
+    const deletedVariedad = await Variedades.deleteOne({ _id: id });
+    
+    res.status(200).json({
+      message: "Variedad eliminada correctamente",
+      deletedVariedad
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al eliminar variedad",
+      error: error.message
+    });
+  }
+});
+
+  
+
+export default variedadesRouter;
