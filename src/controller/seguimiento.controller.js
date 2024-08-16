2be48c1b614fa126aebb9fc5c673f099daaa65a0
LauraB1903 <laurabastidas0319@gmail.com>
2024-08-16 12:42:50 -0500

    arreglo de rutas a controladores

---
+++ src/controller/seguimiento.controller.js
@@ -0,0 +1,126 @@
+export const createSeguimiento = async (req, res) => { 
+
+    const { maquina, loteCafe, operador } = req.body;
+    
+    try {
+
+        const maquinas = await Maquina.findById(maquina);
+        const loteCoffee = await Lotes.findById(loteCafe);
+        const operario = await Usuario.findById(operador);
+
+        if (!maquinas) return res.status(404).json({ message: 'La maquina no existe.' }); 
+        if (!loteCoffee) return res.status(404).json({ message: 'El lote de cafe no existe.' }); 
+        if (!operario) return res.status(404).json({ message: 'El operador no existe.' });
+
+        if (operario.tipoUsuario !== "Operario" && operario.tipoUsuario !== "Administrador") return res.status(400).json({ message: "El usuario no tiene permiso para crear un proceso." });
+
+        if(maquinas.estado !== "Inactivo") return res.status(400).json({ message: `La maquina: ${maquinas.nombre} no esta disponible por el momento.` });
+
+        await Maquina.findByIdAndUpdate(maquina, { estado: "Activo" });
+
+        const seguimiento = await Seguimiento.create(req.body);
+        res.status(201).json({ message: "Seguimiento creado con exito.", seguimiento }); 
+
+    } catch (error) {
+        res.status(500).json({
+            error: error.message,
+            message: "Error al crear el seguimiento."
+        });
+    }
+}
+
+export const getAllSeguimiento = async (req, res) => {
+    try {
+
+        const seguimientos = await Seguimiento.find({})
+            .populate('maquina') 
+            .populate('loteCafe')
+            .populate({
+                path: 'operador',
+                select: '-password'
+            }).populate('datos');
+
+        res.status(200).json({ message: "Seguimientos encontrados con exito.", seguimientos });
+
+    } catch (error) {
+        res.status(500).json({
+            error: error.message,
+            message: "Error al traer los seguimientos."
+        });
+    }
+}
+
+export const getSeguimientoById = async (req, res) => {
+    
+    const { id } = req.params;
+    
+    try {
+
+        const seguimiento = await Seguimiento.findById(id)
+            .populate('maquina')
+            .populate('loteCafe')
+            .populate({
+                path: 'operador',
+                select: '-password'
+            });
+
+        if(!seguimiento) return res.status(404).json({ message: `El seguimiento con id: ${id} no existe.` });
+
+        res.status(200).json({ message: "Seguimiento encontrado exitosamente.", seguimiento });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const updatedSeguimientoById =async (req, res) => {
+
+    const { id } = req.params;
+    const { maquina, loteCafe, operador } = req.body;
+
+    try {
+
+        const seguimiento = await Seguimiento.findById(id);
+        if(!seguimiento) return res.status(404).json({ message: `El seguimiento con id: ${id} no existe.` });
+
+        const machine = await Maquina.findById(maquina);
+        const loteCoffee = await Lotes.findById(loteCafe);
+        const operario = await Usuario.findById(operador);
+
+        if(!machine) return res.status(404).json({ message: `La maquina con id: ${maquina} no existe.` });
+        if(!loteCafe) return res.status(404).json({ message: `El loteCafe con id: ${loteCoffee} no existe.` });
+        if(!operario) return res.status(404).json({ message: `El operario con id: ${operador} no existe.` });
+
+        const updatedSeguimiento = await Seguimiento.findByIdAndUpdate(id, { maquina, loteCafe, operador }, { new:true });
+
+        res.status(200).json({ message: "Seguimiento actualizado exitosamente.", updatedSeguimiento });
+
+    }catch (error) {
+        res.status(500).json({
+            error: error.message,
+            message: "Error al actualizar el seguimiento."
+        });
+    }
+}
+
+export const deleteSeguimientoId = async (req, res) => {
+    
+    const { id } = req.params;
+    
+    try{
+        
+        const seguimiento = await Seguimiento.findById(id);
+        if(!seguimiento) return res.status(404).json({ message: `El seguimiento con id: ${id} no existe.` });
+
+        const deletedSeguimiento = await Seguimiento.findByIdAndDelete(id);
+        await Maquina.findByIdAndUpdate(deletedSeguimiento.maquina, { estado: "Inactivo" });
+
+        res.status(200).json({ message: "Seguimiento eliminado con exito.", deletedSeguimiento });
+
+    }catch (error) {
+        res.status(500).json({
+            error: error.message,
+            message: `Erro al eliminar el seguimiento con id: ${id}`
+        });
+    }
+}
---
+++ src/controller/variedad.controller.js
@@ -0,0 +1,120 @@
+export const createVariedad = async (req, res) => {
+    const {nombre} = req.body;
+  
+      try {
+        const variedad = await Variedades.create({nombre});
+  
+        res.status(201).json({
+          message: "Variedad creada correctamente",
+          variedad
+        });
+  
+      }catch (error) {
+        res.status(500).json({ 
+          message:"Error al crear la variedad",
+          error: error.message
+        });
+      }
+}
+
+export const getAllVariedad = async (req, res) => {
+
+    try {
+      const variedades = await Variedades.find();
+
+      res.status(200).json({
+        message: "variedades encontradas correctamente",
+        variedades
+      });
+
+    }catch (error) {
+      res.status(500).json({
+        message:"Error al encontrar las variedades",
+        error: error.message
+      });
+    }
+}
+
+export const getVariedadId = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const variedad = await Variedades.findById(id);
+  
+      //verificar si la variedad existe
+      if (!variedad) {
+        return res.status(404).json({ 
+          message: "Variedad no encontrada con ese id"
+        });
+      }
+      
+      res.status(200).json({
+        message: "Variedad encontrada correctamente",
+        variedad
+      });
+  
+    } catch (error) {
+      res.status(500).json({
+        message: "Error al obtener variedad",
+        error: error.message
+      });
+    }
+}
+
+export const updateVariedadId =  async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { nombre } = req.body;
+      
+      // Verificar si la variedad existe
+      const variedadExistente = await Variedades.findById(id);
+  
+      if (!variedadExistente) {
+        return res.status(404).json({
+          message: "ID no encontrado. No se puede actualizar."
+        });
+      }
+  
+      // Actualizar la variedad
+      const updatedVariedad = await Variedades.updateOne({ _id: id }, { $set: { nombre } });
+  
+      res.status(200).json({
+        message: "Variedad actualizada correctamente",
+        updatedVariedad
+      });
+  
+    } catch (error) {
+      res.status(500).json({
+        message: "Error al actualizar variedad",
+        error: error.message
+      });
+    }
+}
+
+export const deleteVariedadId = async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      // Verificar si la variedad existe 
+      const variedadExistente = await Variedades.findById(id);
+  
+      if (!variedadExistente) {
+        return res.status(404).json({
+          message: "ID no encontrado. No se puede eliminar."
+        });
+      }
+  
+      // Eliminar la variedad
+      const deletedVariedad = await Variedades.deleteOne({ _id: id });
+      
+      res.status(200).json({
+        message: "Variedad eliminada correctamente",
+        deletedVariedad
+      });
+  
+    } catch (error) {
+      res.status(500).json({
+        message: "Error al eliminar variedad",
+        error: error.message
+      });
+    }
+  }
